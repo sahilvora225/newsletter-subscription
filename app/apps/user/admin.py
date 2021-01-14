38a6f8aff8a152c0f4e44af7c71a6d293409c1d8
Sahil Vora <sahilvora225@gmail.com>
2021-01-14 11:18:56 +0530

    Completed User and Subscription Model

--- app/apps/user/admin.py
+++ app/apps/user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import User, Subscription
+
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Subscription)
--- app/apps/user/models.py
+++ app/apps/user/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
 from django.db import models
 
+
 # Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, *args, **kwargs):
+        user = self.model(email=self.normalize_email(email), *args, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, *args, **kwargs):
+        user = self.create_user(email, password, *args, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
+class Subscription(models.Model):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
